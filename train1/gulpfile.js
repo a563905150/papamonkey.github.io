9b4fc1b3a5813fb631b552f014d262b187e74d90
LIUWENCAN <pheye@qq.com>
2017-08-04 00:37:38 +0800

    1. 引入font-awesome 2. 联系我们完成 3. 底部完成（清除html,body默认样式)

--- train1/gulpfile.js
+++ train1/gulpfile.js
@@ -15,11 +15,19 @@ gulp.task('serve', function() {
 
 gulp.task('vendor', function() {
     gulp.src([
-        'node_modules/bootstrap/dist/css/bootstrap.min.css',
         'node_modules/bootstrap/dist/js/bootstrap.min.js',
-        'node_modules/jquery/dist/jquery.min.js'
+        'node_modules/jquery/dist/jquery.min.js',
         ])
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist/js/'))
+    gulp.src([
+        'node_modules/font-awesome/fonts/*'
+        ])
+        .pipe(gulp.dest('dist/fonts/'))
+    gulp.src([
+        'node_modules/bootstrap/dist/css/bootstrap.min.css',
+        'node_modules/font-awesome/css/font-awesome.min.css'
+        ])
+        .pipe(gulp.dest('dist/css/'))
 })
 
 gulp.task('sass', function() {
